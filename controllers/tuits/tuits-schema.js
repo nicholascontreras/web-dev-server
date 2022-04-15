44370a4e0295e5c67c57dbee0e56a4690c818656
Nicholas Contreras <nicholasc2000@yahoo.com>
2022-04-15 12:34:38 -0400

    update schema

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,9 +1,16 @@
 import mongoose from 'mongoose';
 const schema = mongoose.Schema({
     tuit: String,
-    likes: Number,
     postedBy: {
         username: String
-    }
+    },
+    stats: {
+        comments: Number,
+        retuits: Number,
+        likes: Number
+    },
+    liked: Boolean,
+    handle: String,
+    "avatar-image": String
 }, {collection: 'tuits'});
 export default schema;
