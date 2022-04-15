0b3aace4c73b17c977024c5b21637897eb0328c7
Nicholas Contreras <nicholasc2000@yahoo.com>
2022-04-15 12:25:46 -0400

    first a9

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from "mongoose";
 
 import helloController from "./controllers/hello-controller.js";
 import userController from "./controllers/users/user-controller.js";
 import tuitsController from "./controllers/tuits/tuits-controller.js";
+
+mongoose.connect('mongodb+srv://webdev:webdev@webdev.1dana.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 
 const app = express();
 app.use(cors());
